c93bf31ca6bdda6363c6a6dccf91bb5bca931691
Kyle Ashmun <--global>
2022-09-23 17:09:06 -0500

    Added player entry to team1

--- src/app/screens/player-entry-screen/player-entry-screen.component.ts
+++ src/app/screens/player-entry-screen/player-entry-screen.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from "rxjs";
 import Player from "src/app/Player";
 import Team from "src/app/Team";
 import { PlayerEntryService } from "src/app/services/player-entry.service";
+import { FormGroup, Validators, FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-player-entry-screen",
@@ -10,15 +11,27 @@ import { PlayerEntryService } from "src/app/services/player-entry.service";
   styleUrls: ["./player-entry-screen.component.css"],
 })
 export class PlayerEntryScreenComponent implements OnInit, OnDestroy {
-  private team1Sub: Subscription;
-  private team2Sub: Subscription;
-
-  team1: Team;
-  team2: Team;
-
   constructor(private playerEntryService: PlayerEntryService) {}
 
+  private team1Sub: Subscription;
+  private team2Sub: Subscription;
+  team1: Team;
+  team2: Team;
+  players: Player[] = new Array();
+  team1Form: FormGroup;
+  team1PlayerNameForm: FormGroup;
+  playerID: FormControl;
+  playerNoExist = false;
+
   ngOnInit(): void {
+    this.team1Form = new FormGroup({
+      playerID: new FormControl("", Validators.required),
+    });
+
+    this.team1PlayerNameForm = new FormGroup({
+      toAdd: new FormControl(''),
+    })
+
     this.team1 = this.playerEntryService.getTeam1();
     this.team2 = this.playerEntryService.getTeam2();
 
@@ -31,14 +44,21 @@ export class PlayerEntryScreenComponent implements OnInit, OnDestroy {
     });
   }
 
-  async add_player(id: number, teamNum: number) {
+  async fetch_player(id: number, teamNum: number) {
     this.playerEntryService.fetchPlayerInfo(id, teamNum).then(
       (result) => {
-        return result;
+        if(!result){
+          console.log(result)
+          this.playerNoExist = true;
+        }
+        else{
+          this.team1Form.reset();
+        }
       },
       (error) => {
         console.log("ERROR: player entry screen -> add_player()");
         console.log(error);
+        return false;
       }
     );
   }
@@ -46,5 +66,35 @@ export class PlayerEntryScreenComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.team1Sub.unsubscribe();
     this.team2Sub.unsubscribe();
+    this.team1Form = new FormGroup({
+      playerAdded: new FormControl("", Validators.required),
+    });
+  }
+
+  enterPlayer(id: number, teamNumber: number) {
+    let newPlayer = new Player(id, this.team1PlayerNameForm.controls.toAdd.value);
+    this.team1.players[id] = newPlayer;
+    this.playerNoExist = false;
+    this.team1Form.reset();
+    console.log(this.team1);
+  }
+
+  removePlayer(id: number, teamNumber: number) {
+    console.log(id);
+    console.log(this.team1.players);
+    if(teamNumber === 1)
+    {
+      this.team1.players[id] = null;
+    }
+    
+  }
+
+  startGame() {
+    let count = 0;
+    const result = this.players.filter((element): element is Player => {
+      return element !== null;
+    });
+    console.log("start game works (create call here)");
+    console.log(result);
   }
 }
